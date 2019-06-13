c607234091d94f851557c1b9d05a24f42132da42
Palash Gupta <palashg7563@gmail.com>
2019-06-13 11:49:13 +0530

    questions are added

---
+++ public/data/question.js
@@ -0,0 +1,46 @@
+export const question = [
+  {
+    question: "Which company created  the Javascript",
+    option: ["Netscape", "Mozilla", "Apple", "Microsoft"],
+    hint: "Firefox...?",
+    correctoption: 1
+  },
+  {
+    question:
+      "What is the ouput of the program\nvar x = 10;\n x = x + 5; \nconsole.log(x);",
+    option: [15, 10, 5, -10],
+    hint: "1+2=3",
+    correctoption: 0
+  },
+  {
+    question: "What is the output of the program \nconsole.log(-5/0);",
+    option: [0, "- Infinity", "+ Infinity", "None of the above"],
+    hint: "",
+    correctoption: 1
+  },
+  {
+    question:
+      "What is the output of the program \nconsole.log(3+5+&quot;5&quot;);",
+    option: [355, 13, 85, "None of the above"],
+    hint: "Think about the coercion",
+    correctoption: 2
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    option: ["<script>", "<js>", "<javascript>", "<scripting>"],
+    hint: "Javascript is a scripting language which runs on scripts",
+    correctoption: 0
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called &quot;xxx.js&quot;?",
+    option: [
+      "<script name = “xxx.js”>",
+      "<script href = “xxx.js”>",
+      "<script src=”xxx.js”>",
+      "<script import=”xxx.js”>"
+    ],
+    hint: "Javascript is a scripting language which runs on scripts",
+    correctoption: 0
+  }
+];
