f35f2db807eb24d0fc04f91447f3e4c48d1c5404
Palash Gupta <palashg7563@gmail.com>
2019-06-14 13:05:55 +0530

    edge case is handled when the db in null

--- public/js/nickname.js
+++ public/js/nickname.js
@@ -14,22 +14,36 @@ async function nameRegister() {
 
     const res = await users;
 
-    const user = Object.values(res);
-    const userIndex = user.findIndex(e => e.player.name === nickname);
-    if (userIndex === -1) {
-      // new user is created when the name is not in the db
+    if (res != null) {
+      const user = Object.values(res);
+      const userIndex = user.findIndex(e => e.name === nickname);
+      if (userIndex === -1) {
+        // new user is created when the name is not in the db
+        const key = stuRef.push().key;
+        stuRef.child(key).set({
+          name: nickname,
+          coins: 0
+        });
+        $("#firebase").modal("toggle");
+        game.scene.start("PlayGame", {
+          data: { name: nickname, coins: 0 }
+        });
+      } else {
+        //retreive the data from the user and started the game
+        $("#firebase").modal("toggle");
+        const data = user[userIndex];
+        game.scene.start("PlayGame", { data });
+      }
+    } else {
       const key = stuRef.push().key;
       stuRef.child(key).set({
         name: nickname,
         coins: 0
       });
       $("#firebase").modal("toggle");
-      game.scene.start("PlayGame", { data: { name: nickname, coins: 0 } });
-    } else {
-      //retreive the data from the user and started the game
-      $("#firebase").modal("toggle");
-      const data = user[userIndex].player;
-      game.scene.start("PlayGame", { data });
+      game.scene.start("PlayGame", {
+        data: { name: nickname, coins: 0 }
+      });
     }
   }
 }
